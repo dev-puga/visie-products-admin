b86aca6cdbff14c3b7a38e8fb55dc18fea0e5c3e
Puga <willian.puga@gmail.com>
2023-10-22 12:28:33 -0300

    fix: Add style for product list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,44 +13,47 @@ export const Home = () => {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Lista de Produtos</h1>
-      <ul className="bg-white shadow overflow-hidden sm:rounded-md">
-        {products.map((product) => (
-          <li key={product.id} className="border-t border-gray-200">
-            <div className="px-4 py-4 sm:px-6">
-              <div className="flex items-center">
-                <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
-                  <div>
-                    <p className="text-sm font-medium text-indigo-600 truncate">
-                      {product.title}
-                    </p>
-                  </div>
-                  <div className="mt-2 flex justify-center space-x-2">
-                    <a
-                      href={`/product/${product.id}`}
-                      className="text-indigo-600 hover:text-indigo-900"
-                    >
-                      Ver Dados
-                    </a>
-                    <a
-                      href={`/product/${product.id}/edit`}
-                      className="text-indigo-600 hover:text-indigo-900"
-                    >
-                      Alterar
-                    </a>
-                    <button
-                      className="text-red-600 hover:text-red-900 cursor-pointer"
-                      onClick={() => handleDeleteProduct(product.id)}
-                    >
-                      Excluir
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <h1 className="text-2xl text-center font-bold mb-4">Lista de Produtos</h1>
+      <div className="w-full overflow-x-auto">
+        <table className="w-full table-auto">
+          <thead className="text-start border-b">
+            <tr>
+              <th className="text-start p-2 sm:p-4">Nome do Produto</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id} className="border-b hover:bg-gray-100">
+                <td className="p-2 sm:p-4">{product.title}</td>
+                <td className="p-2 sm:p-4">
+                  <a
+                    href={`/product/${product.id}`}
+                    className="text-indigo-600 hover:text-indigo-900"
+                  >
+                    Ver Dados
+                  </a>
+                </td>
+                <td className="p-2 sm:p-4">
+                  <a
+                    href={`/product/${product.id}/edit`}
+                    className="text-indigo-600 hover:text-indigo-900"
+                  >
+                    Alterar
+                  </a>
+                </td>
+                <td className="p-2 sm:p-4">
+                  <button
+                    className="text-red-600 hover:text-red-900 cursor-pointer"
+                    onClick={() => handleDeleteProduct(product.id)}
+                  >
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
